9ac1f3bf2d0dcc4dfb25d99815d645d45dbc27a2
Patrick Ruckstuhl <patrick@ch.tario.org>
2017-10-10 22:28:48 +0200

    working version

--- bapi/api/query.py
+++ bapi/api/query.py
@@ -1,13 +1,16 @@
 from bapi.api.core import api
 from flask_restplus.resource import Resource
-from bapi.core.beancount import beancount
+from beancount import loader
+from beancount.query import query
+from bapi.core import config
+from bapi.core import storage
 
 ns = api.namespace('query', description='Operations related to bean-query')
 
 @ns.route('/')
 class Query(Resource):
     def get(self):
-        types, rows = beancount.runQuery('select position')
+        types, rows = storage.storage.runQuery('select position')
         
         res = []
         for row in rows:
--- bapi/core/storage.py
+++ bapi/core/storage.py
@@ -2,7 +2,7 @@ from beancount import loader
 from beancount.query import query
 from bapi.core import config
 
-class Beancount(object):
+class Storage:
     def __init__(self, fileName):
         self.fileName = fileName
         self.entries, self.errors, self.options = loader.load_file(fileName)
@@ -10,4 +10,4 @@ class Beancount(object):
     def runQuery(self, queryString):
         return query.run_query(self.entries, self.options, queryString)
 
-beancount = Beancount(config.filename)
+storage = Storage('dummy.beancount')
