2d35d44b6de47e8c1d55781bd95309d506ad0c69
Patrick Ruckstuhl <patrick@ch.tario.org>
2017-10-18 20:14:44 +0200

    moved port/host/debug to cli options

--- bapi/app.py
+++ bapi/app.py
@@ -7,10 +7,16 @@ from bapi.core.storage import storage
 from bapi import __version__
 
 @click.command()
+@click.option('-p', '--port', type=int, default=1234,
+              help='The port to listen on. (default: 1234)')
+@click.option('-H', '--host', type=str, default='localhost',
+              help='The host to listen on. (default: localhost)')
+@click.option('-d', '--debug', is_flag=True,
+              help='Turn on debugging.')
 @click.argument('filename',
                 type=click.Path(exists=True, resolve_path=True))
 @click.version_option(version=__version__, prog_name='bapi')
-def main(filename):
+def main(filename, port, host, debug):
     storage.load(filename)
     
     app = Flask(__name__)
@@ -18,7 +24,7 @@ def main(filename):
     api.add_namespace(query_ns)
     api.init_app(app)
 
-    app.run(port=1234, debug=True, threaded=True)
+    app.run(host=host, port=port, debug=debug, threaded=True)
 
 if __name__ == '__main__':
     main()
