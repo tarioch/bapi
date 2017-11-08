99afdea9e9ada861d093d1c69c90d1da562d9d2f
Patrick Ruckstuhl <patrick@ch.tario.org>
2017-11-08 20:56:51 +0100

    adjust test

--- tests/core/storage_test.py
+++ tests/core/storage_test.py
@@ -3,22 +3,22 @@ import pytest
 from bapi.core.exception import BapiException
 
 def test_load_ok():
-    storage.load('tests/data/example.beancount')
+    storage.load('tests/data', 'example.beancount')
 
 def test_load_not_exists():
     with pytest.raises(BapiException):
-        storage.load('tests/data/nosuchfile.beancout')
+        storage.load('tests/data', 'nosuchfile.beancout')
 
 def test_query_ok():
-    storage.load('tests/data/example.beancount')
+    storage.load('tests/data', 'example.beancount')
     storage.runQuery('select position, currency')
 
 def test_query_bad_syntax():
     with pytest.raises(BapiException):
-        storage.load('tests/data/example.beancount')
+        storage.load('tests/data', 'example.beancount')
         storage.runQuery('invalid')
 
 def test_query_invalid_field():
     with pytest.raises(BapiException):
-        storage.load('tests/data/example.beancount')
+        storage.load('tests/data', 'example.beancount')
         storage.runQuery('select invalid')
